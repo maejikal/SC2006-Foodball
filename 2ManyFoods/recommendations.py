3460f2de0d728408e86d97056c680bb09f82124d
Ansel-Kee <a1.kee.jx@gmail.com>
2025-10-11 00:40:16 +0800

    Update recommendations.py

--- 2ManyFoods/recommendations.py
+++ 2ManyFoods/recommendations.py
@@ -10,6 +10,33 @@ load_dotenv()
 
 API_KEY = os.getenv('GMAPS_API_KEY')
 
+def _fetch_remote_json(service_url, params=None):
+    encoded_data = {}
+    for k, v in params.items():
+        v = v.encode('utf-8')
+        encoded_data[k] = v
+    encoded_data = urllib.parse.urlencode(encoded_data)
+
+    request_url = service_url + encoded_data
+    request = urllib.request.Request(request_url, data=encoded_data)
+
+    response = urllib.request.urlopen(request)
+    str_response = response.read().decode('utf-8')
+    return (request_url, json.loads(str_response, parse_float=Decimal)['places'])
+
+def nearby_search(lat_lng=None, radius=3200, type='restaurant'):
+    lat_lng_str = str(lat_lng["lat"]) + "," + str(lat_lng["lng"])
+    params = {'location': lat_lng_str,
+              'radius': radius,
+              'type': type,
+              'language': 'en',
+              'sensor': 'false',
+              'key': API_KEY
+              }
+    url, places_response = _fetch_remote_json(
+        'https://maps.googleapis.com/maps/api/place/nearbysearch/json?', params)
+    return [i for i in places_response["results"]]
+
 class RecommendationController:
     def __init__(self, group: Group, location: Location, radius: int = 500):
         self.group = group
@@ -20,7 +47,12 @@ class RecommendationController:
         query_result = nearby_search(
             lat_lng={'lat': location.latitude, 'lng': location.longitude},
             radius=radius, types=['restuarant'])
-        return [Eatery(i) for i in query_result]
+        results = [Eatery(i) for i in query_result]
+        # TODO
+        # filter further based on hunger levels and preferences, then look in the 'types' field in each result
+        # https://developers.google.com/maps/documentation/places/web-service/reference/rest/v1/places
+        # https://developers.google.com/maps/documentation/places/web-service/place-types
+        return results
 
     def GroupVoting(self, group: Group) -> int:
         votes = {i: 0 for i in self.recommendations}
@@ -37,35 +69,3 @@ class RecommendationInterface:
 
     def VoteAndDisplay():
         pass
-
-
-def _fetch_remote_json(service_url, params=None, use_http_post=False):
-    request_url, response = _fetch_remote(service_url, params, use_http_post)
-    str_response = response.read().decode('utf-8')
-    return (request_url, json.loads(str_response, parse_float=Decimal))
-
-
-def _fetch_remote(service_url, params=None, use_http_post=False):
-    encoded_data = {}
-    for k, v in params.items():
-        v = v.encode('utf-8')
-        encoded_data[k] = v
-    encoded_data = urllib.parse.urlencode(encoded_data)
-
-    request_url = service_url + encoded_data
-    request = urllib.request.Request(request_url)
-    return (request_url, urllib.request.urlopen(request))
-
-
-def nearby_search(lat_lng=None, radius=3200, type='restaurant'):
-    lat_lng_str = str(lat_lng["lat"]) + "," + str(lat_lng["lng"])
-    params = {'location': lat_lng_str,
-              'radius': radius,
-              'type': type,
-              'language': 'en',
-              'sensor': 'false',
-              'key': API_KEY
-              }
-    url, places_response = _fetch_remote_json(
-        'https://maps.googleapis.com/maps/api/place/nearbysearch/json?', params)
-    return [i for i in places_response["results"]]
