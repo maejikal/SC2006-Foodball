f3b407f9375d2c949e6a01a5ce392ca78013a72e
zhong liang <ztan127@e.ntu.edu.sg>
2025-10-16 01:00:32 +0800

    Add new routes for account security and preferences

--- 2ManyFoods/2manyfoods-frontend/src/App.jsx
+++ 2ManyFoods/2manyfoods-frontend/src/App.jsx
@@ -7,6 +7,12 @@ import SignupPage from './pages/SignupPage';
 import DashboardPage from './pages/DashboardPage';
 import SearchPage from './pages/SearchPage';
 import AccountPage from './pages/AccountPage';
+import SecurityPage from './pages/SecurityPage';
+import ChangePasswordPage from './pages/ChangePasswordPage';
+import DietaryPreferencesPage from './pages/DietaryPreferencesPage';
+import CuisinePreferencesPage from './pages/CuisinePreferencesPage';
+import FoodHistoryPage from './pages/FoodHistoryPage';
+import FoodReviewPage from './pages/FoodReviewPage';
 import GroupsPage from './pages/GroupsPage';
 import CreateGroupPage from './pages/CreateGroupPage';
 import GroupDetailsPage from './pages/GroupDetailsPage';
@@ -24,6 +30,12 @@ function App() {
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/search" element={<SearchPage />} />
         <Route path="/account" element={<AccountPage />} />
+        <Route path="/account/security" element={<SecurityPage />} />
+        <Route path="/account/security/change-password" element={<ChangePasswordPage />} />
+        <Route path="/account/dietary" element={<DietaryPreferencesPage />} />
+        <Route path="/account/cuisine" element={<CuisinePreferencesPage />} />
+        <Route path="/account/history" element={<FoodHistoryPage />} />
+        <Route path="/account/review" element={<FoodReviewPage />} />
         <Route path="/groups" element={<GroupsPage />} />
         <Route path="/groups/create" element={<CreateGroupPage />} />
         <Route path="/groups/:groupId" element={<GroupDetailsPage />} />
