28c43bb986fa61a8f58b4866962a39d3e8d774a9
Venixin <87964453+Venixin@users.noreply.github.com>
2025-10-29 16:29:30 +0800

    PLEASE

--- 2ManyFoods/utils/verification.py
+++ 2ManyFoods/utils/verification.py
@@ -3,7 +3,6 @@ from random import randint
 def send_email(app, email):
     msg = Message(
         subject = "Your 2ManyFoods Verification Code",
-        sender = "noreply@2manyfood.com",
         recipient = email
     )
     code = str(randint(0,999999))
@@ -11,5 +10,10 @@ def send_email(app, email):
         code = "0" + code
     msg.body = "Your verification code is " + code
     mail = Mail(app)
+    app.config["MAIL_SERVER"] = "smtp.gmail.com"
+    app.config["MAIL_PORT"] = 587
+    app.config["MAIL_USE_SSL"] = True
+    app.config["MAIL_USERNAME"] = 'noreply@2manyfood.com'  
+    app.config["MAIL_PASSWORD"] = 'password'
     mail.connect().send(msg)
     return code
